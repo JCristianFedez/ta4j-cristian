7a343b4926045cb344f2259d428d0cb3875222c7
JCristianFedez <j.cristian.fedez@gmail.com>
2022-12-03 20:48:02 +0100

    refactor: Reverse operation order

--- ta4j-core/src/test/java/org/ta4j/core/indicators/stdfilterednpolegaussian/STDFilteredNPoleGaussianFilterTest.java
+++ ta4j-core/src/test/java/org/ta4j/core/indicators/stdfilterednpolegaussian/STDFilteredNPoleGaussianFilterTest.java
@@ -4,10 +4,9 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
-import java.time.ZoneOffset;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,80 +32,79 @@ public class STDFilteredNPoleGaussianFilterTest {
   @Test
   public void testIndicator() throws ParseException {
     final List<Candlestick> candlesticks = CandlestickLoader.btcUsdt();
-    Collections.reverse(candlesticks);
 
     final List<Operation> operations = STDFilteredNPoleGaussianFilter.of(buildBarSerie(candlesticks), CURRENCY_PAIR).execute();
 
     Assert.assertEquals(902, operations.size());
     Assert.assertEquals(849, operations.stream().map(Operation::getType).filter(OperationType.NONE::equals).count());
-    Assert.assertEquals(expBuys(), findOperationByTipe(operations, OperationType.BUY));
-    Assert.assertEquals(expSells(), findOperationByTipe(operations, OperationType.SELL));
+    Assert.assertEquals(expBuys(), findOperationByType(operations, OperationType.BUY));
+    Assert.assertEquals(expSells(), findOperationByType(operations, OperationType.SELL));
   }
 
   private List<Operation> expBuys() throws ParseException {
     return Arrays.asList(
-        buildBuyOperation("2022-11-10 22:00:00.0"),
-        buildBuyOperation("2022-11-10 14:45:00.0"),
-        buildBuyOperation("2022-11-10 02:00:00.0"),
-        buildBuyOperation("2022-11-09 06:45:00.0"),
-        buildBuyOperation("2022-11-08 23:45:00.0"),
-        buildBuyOperation("2022-11-08 17:30:00.0"),
-        buildBuyOperation("2022-11-08 02:45:00.0"),
-        buildBuyOperation("2022-11-07 20:30:00.0"),
-        buildBuyOperation("2022-11-07 17:30:00.0"),
-        buildBuyOperation("2022-11-07 11:45:00.0"),
-        buildBuyOperation("2022-11-07 04:15:00.0"),
-        buildBuyOperation("2022-11-06 16:45:00.0"),
-        buildBuyOperation("2022-11-06 10:00:00.0"),
-        buildBuyOperation("2022-11-06 02:30:00.0"),
-        buildBuyOperation("2022-11-05 21:00:00.0"),
-        buildBuyOperation("2022-11-05 18:45:00.0"),
-        buildBuyOperation("2022-11-05 12:45:00.0"),
-        buildBuyOperation("2022-11-04 20:30:00.0"),
-        buildBuyOperation("2022-11-04 14:00:00.0"),
-        buildBuyOperation("2022-11-04 02:00:00.0"),
-        buildBuyOperation("2022-11-03 22:45:00.0"),
-        buildBuyOperation("2022-11-03 16:00:00.0"),
-        buildBuyOperation("2022-11-03 02:00:00.0"),
-        buildBuyOperation("2022-11-02 16:30:00.0"),
-        buildBuyOperation("2022-11-02 10:45:00.0"),
+        buildBuyOperation("2022-11-01 20:30:00.0"),
         buildBuyOperation("2022-11-02 00:45:00.0"),
-        buildBuyOperation("2022-11-01 20:30:00.0")
+        buildBuyOperation("2022-11-02 10:45:00.0"),
+        buildBuyOperation("2022-11-02 16:30:00.0"),
+        buildBuyOperation("2022-11-03 02:00:00.0"),
+        buildBuyOperation("2022-11-03 16:00:00.0"),
+        buildBuyOperation("2022-11-03 22:45:00.0"),
+        buildBuyOperation("2022-11-04 02:00:00.0"),
+        buildBuyOperation("2022-11-04 14:00:00.0"),
+        buildBuyOperation("2022-11-04 20:30:00.0"),
+        buildBuyOperation("2022-11-05 12:45:00.0"),
+        buildBuyOperation("2022-11-05 18:45:00.0"),
+        buildBuyOperation("2022-11-05 21:00:00.0"),
+        buildBuyOperation("2022-11-06 02:30:00.0"),
+        buildBuyOperation("2022-11-06 10:00:00.0"),
+        buildBuyOperation("2022-11-06 16:45:00.0"),
+        buildBuyOperation("2022-11-07 04:15:00.0"),
+        buildBuyOperation("2022-11-07 11:45:00.0"),
+        buildBuyOperation("2022-11-07 17:30:00.0"),
+        buildBuyOperation("2022-11-07 20:30:00.0"),
+        buildBuyOperation("2022-11-08 02:45:00.0"),
+        buildBuyOperation("2022-11-08 17:30:00.0"),
+        buildBuyOperation("2022-11-08 23:45:00.0"),
+        buildBuyOperation("2022-11-09 06:45:00.0"),
+        buildBuyOperation("2022-11-10 02:00:00.0"),
+        buildBuyOperation("2022-11-10 14:45:00.0"),
+        buildBuyOperation("2022-11-10 22:00:00.0")
     );
   }
 
   private List<Operation> expSells() throws ParseException {
     return Arrays.asList(
-        buildSellOperation("2022-11-10 19:45:00.0"),
-        buildSellOperation("2022-11-10 11:30:00.0"),
-        buildSellOperation("2022-11-09 09:30:00.0"),
-        buildSellOperation("2022-11-09 02:30:00.0"),
-        buildSellOperation("2022-11-08 19:30:00.0"),
-        buildSellOperation("2022-11-08 04:45:00.0"),
-        buildSellOperation("2022-11-07 23:30:00.0"),
-        buildSellOperation("2022-11-07 18:30:00.0"),
-        buildSellOperation("2022-11-07 15:00:00.0"),
-        buildSellOperation("2022-11-07 04:45:00.0"),
-        buildSellOperation("2022-11-06 19:30:00.0"),
-        buildSellOperation("2022-11-06 13:45:00.0"),
-        buildSellOperation("2022-11-06 03:15:00.0"),
-        buildSellOperation("2022-11-06 00:15:00.0"),
-        buildSellOperation("2022-11-05 20:15:00.0"),
-        buildSellOperation("2022-11-05 14:45:00.0"),
-        buildSellOperation("2022-11-05 07:45:00.0"),
-        buildSellOperation("2022-11-04 19:00:00.0"),
-        buildSellOperation("2022-11-04 13:15:00.0"),
-        buildSellOperation("2022-11-03 23:45:00.0"),
-        buildSellOperation("2022-11-03 19:15:00.0"),
-        buildSellOperation("2022-11-03 08:45:00.0"),
-        buildSellOperation("2022-11-02 21:00:00.0"),
-        buildSellOperation("2022-11-02 11:45:00.0"),
+        buildSellOperation("2022-11-02 00:00:00.0"),
         buildSellOperation("2022-11-02 07:15:00.0"),
-        buildSellOperation("2022-11-02 00:00:00.0")
+        buildSellOperation("2022-11-02 11:45:00.0"),
+        buildSellOperation("2022-11-02 21:00:00.0"),
+        buildSellOperation("2022-11-03 08:45:00.0"),
+        buildSellOperation("2022-11-03 19:15:00.0"),
+        buildSellOperation("2022-11-03 23:45:00.0"),
+        buildSellOperation("2022-11-04 13:15:00.0"),
+        buildSellOperation("2022-11-04 19:00:00.0"),
+        buildSellOperation("2022-11-05 07:45:00.0"),
+        buildSellOperation("2022-11-05 14:45:00.0"),
+        buildSellOperation("2022-11-05 20:15:00.0"),
+        buildSellOperation("2022-11-06 00:15:00.0"),
+        buildSellOperation("2022-11-06 03:15:00.0"),
+        buildSellOperation("2022-11-06 13:45:00.0"),
+        buildSellOperation("2022-11-06 19:30:00.0"),
+        buildSellOperation("2022-11-07 04:45:00.0"),
+        buildSellOperation("2022-11-07 15:00:00.0"),
+        buildSellOperation("2022-11-07 18:30:00.0"),
+        buildSellOperation("2022-11-07 23:30:00.0"),
+        buildSellOperation("2022-11-08 04:45:00.0"),
+        buildSellOperation("2022-11-08 19:30:00.0"),
+        buildSellOperation("2022-11-09 02:30:00.0"),
+        buildSellOperation("2022-11-09 09:30:00.0"),
+        buildSellOperation("2022-11-10 11:30:00.0"),
+        buildSellOperation("2022-11-10 19:45:00.0")
     );
   }
 
-  private List<Operation> findOperationByTipe(final List<Operation> operations, final OperationType buy) {
+  private List<Operation> findOperationByType(final List<Operation> operations, final OperationType buy) {
     return operations.stream().filter(operation -> buy.equals(operation.getType())).collect(Collectors.toList());
   }
 
@@ -138,7 +136,7 @@ public class STDFilteredNPoleGaussianFilterTest {
   }
 
   private Bar buildBar(final Candlestick candlestick) {
-    ZonedDateTime time = ZonedDateTime.ofInstant(Instant.ofEpochMilli(candlestick.getOpenTime()), ZoneOffset.UTC);
+    final ZonedDateTime time = ZonedDateTime.ofInstant(Instant.ofEpochMilli(candlestick.getOpenTime()), ZoneId.systemDefault());
     return new BaseBar(Duration.ofMinutes(15), time, 0, 0, 0, Double.parseDouble(candlestick.getClose()), 0);
   }
 }
